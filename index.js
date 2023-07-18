d49b79423668cacad42c91a936c4c131a1e3bd68
Davi <88855441+davi12345452@users.noreply.github.com>
2023-07-18 09:19:35 -0300

    agora sim está configurado

--- index.js
+++ index.js
@@ -11,23 +11,10 @@ app.set('view engine', 'ejs');
 // Aqui estou permitindo o uso na pasta public
 app.use(express.static("public"));
 
-let _array = [{tipo: "Futebol", marca: "Nike"}, {tipo: "Volei", marca: "Adidas"}, {tipo: "Basquete", marca: "Puma"}]
-
 //Rota teste para incializar
-app.get("/:nome?", (req, res) => {
-    // Usar condicional no .ejs: basta colocar o elemento html entre a seguinte estrutura:
-    // <%if(testCond){%> oekfobofwb <%}%>, o <%= é usado somente com variáveis.
-    let testCond = true;
-    let nome = req.params.nome;
-    let s_nome = "Sem nome";
-    // Renderizando o ejs com variáveis (podendo vir de requisoções)
-    // basta chamar no .ejs um termo <%= <var> %>
-    if(nome) {
-        res.render('../view/index.ejs', {nome: nome, condicional: testCond, arr: _array});
-    }else{
-        res.render('../view/index.ejs', {nome: s_nome, condicional: testCond, arr: _array});
-    }
-}); // Permite use de arrow functions
+app.get("/", (req, res) => {
+    res.render('../view/index.ejs');
+});
 
 // Inicializar a aplicação
 
